8f14ec5b4c2ab0e8e9d7a888a2463937da7fa865
NuminousBlue <jdiehl2236@gmail.com>
2022-02-09 14:39:58 -0600

    generating dynamodb tables programmatically

---
+++ server/db/ThoughtsTable.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk');
+
+// = Configure connection to local dynamoDB instance = //
+AWS.config.update({
+   region: "us-east-1",
+   endpoint: "http://localhost:8000"
+});
+
+// = Use aws class to build object to interface with DB = //
+const dynamodb = new AWS.DynamoDB({
+   apiVersion: '2012-08-10'
+});
+
+// = Define parameters for createTable method = //
+const params = {
+   TableName: "Thoughts",
+   KeySchema: [{
+         AttributeName: "username",
+         KeyType: "HASH"
+      }, // Partition key
+      {
+         AttributeName: "createdAt",
+         KeyType: "RANGE"
+      } // Sort key
+   ],
+   AttributeDefinitions: [{
+         AttributeName: "username",
+         AttributeType: "S"
+      },
+      {
+         AttributeName: "createdAt",
+         AttributeType: "N"
+      }
+   ],
+   ProvisionedThroughput: {
+      ReadCapacityUnits: 10,
+      WriteCapacityUnits: 10
+   }
+};
+
+dynamodb.createTable(params, (err, data) => {
+   if (err) {
+      console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+   } else {
+      console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+   }
+});
