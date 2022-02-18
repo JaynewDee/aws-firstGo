f7cf818a67a8119f3196f414d1005c540ea0e7cc
NuminousBlue <jdiehl2236@gmail.com>
2022-02-18 17:33:19 -0600

    post ponderance route built and functional

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -67,4 +67,27 @@ router.get('/users/:username', (req, res) => {
 
  });
 
+ // Create new user at /api/users
+router.post('/users', (req, res) => {
+   const params = {
+     TableName: table,
+      // - data object from client - //
+     Item: {
+       "username": req.body.username,
+       "createdAt": Date.now(),
+       "ponderance": req.body.ponderance
+     }
+   };
+   // - put method corresponds with functionality of post route - //
+   dynamodb.put(params, (err, data) => {
+      if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).json(err);
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+        res.json({"Added": JSON.stringify(data, null, 2)});
+      }
+    });
+});  
+
 module.exports = router;
