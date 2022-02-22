7dd6ec10ffab7493390e969d8105ddda9463d3a3
NuminousBlue <jdiehl2236@gmail.com>
2022-02-22 01:08:44 -0600

    configured s3 bucket to accept images on form submit

---
+++ server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const imageParams = fileName => {
+   const myFile = fileName.originalname.split('.');
+   console.log(myFile)
+   const fileType = myFile[myFile.length - 1];
+
+   const imageParams = {
+      Bucket: 'user-images-2e372f2e-1764-4799-a190-901356166918',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+   };
+
+   return imageParams;
+}
+
+module.exports =  imageParams;
